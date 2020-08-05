ac6da530a84c9d5854244a5ac8fe3d455cbc2f27
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-05 16:54:30 +0200

    Snack 3

---
+++ snack-3/js/script.js
@@ -0,0 +1,13 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+// Chiedere all'utente di inserire un numero
+var numero = parseInt(prompt("Inserisci un numero"));
+
+/*  Se numero % 2 == 0 stampo numero
+    Altrimenti stampo (numero + 1)
+*/
+if (numero % 2 == 0) {
+  console.log(numero);
+}else {
+  console.log(numero + 1);
+}
